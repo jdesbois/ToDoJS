2284b439b3f2e7086fbbabc98c6416cabbb40363
John Desbois <jdesbois@me.com>
2018-04-23 20:14:47 +0100

    updated add/remove functions - started css

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
+
+//Global Variables
 const toDoList = document.getElementById("toDoList");
 const toDoInput = document.getElementById("toDoInput");
 
-
-
+//Hit enter Key to input item
+toDoInput.addEventListener('keydown', function(){
+    if (event.key == "Enter") {
+        addToDoItem();
+    }
+})
+//Select text input value and add to list
 function addToDoItem() {
     let listItem = toDoInput.value;
     let li = document.createElement('li');
     li.setAttribute('class', 'toDoItem');
-    li.innerHTML = listItem + '<input type="button" value="done" onclick="finishToDo()">';
+    li.setAttribute('onclick', 'finishToDo()');
+    li.innerHTML = listItem;
     toDoList.appendChild(li);
+    toDoInput.value = "";
 };
-
+//remove selected item
 function finishToDo() {
-    console.log(toDoList);
-    
+    event.target.remove(this);
 }
